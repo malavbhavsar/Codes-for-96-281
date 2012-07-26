385ad871663fe37a03d4842878041a061b48404c
Malav bhavsar <malav.bhavsar@gmail.com>
2012-07-26 17:40:46 +0530

    Code to pass cosm_parser_spec.rb and checking out a real stream
    I don't know how to access the ENV hash which is used in cosm_stream.rb
    So, jus replaced ENV["PACHUBE_API_KEY"] with my key.

--- cosm-ruby/02_cosm/cosm_parser.rb
+++ cosm-ruby/02_cosm/cosm_parser.rb
@@ -3,7 +3,20 @@ require "uri"
 require 'json'
 
 class CosmParser
-
-
+  
+  API_KEY = "e0wbVb726nWadWuERwigaZDuU3OSAKxqWFgzTGRkUFpuND0g"
+  
+  def self.get_feed(feed_id)
+    uri = URI.parse("http://api.cosm.com/v2/feeds/"+feed_id)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request["X-ApiKey"] = API_KEY
+    response = http.request(request)
+    response.body
+  end
+  
+  def self.convert_json_to_ruby_object(json_string)
+    JSON.parse(json_string)
+  end
 end
 
