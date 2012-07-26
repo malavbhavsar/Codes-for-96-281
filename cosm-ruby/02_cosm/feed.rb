11a1bc3a6fdd4e5f90c8bd6238f36f893dbac2be
Malav bhavsar <malav.bhavsar@gmail.com>
2012-07-26 17:06:59 +0530

    Code to pass feed_spec.rb
    added autotest in gemfile, but it's not working.

--- cosm-ruby/02_cosm/feed.rb
+++ cosm-ruby/02_cosm/feed.rb
@@ -5,9 +5,46 @@ class Feed
   def initialize(input)
     case input
       when String
+        @input = JSON.parse(input)
       when Hash
+        @input = input
     end
   end
-
-
+  
+  attr_accessor :input
+  
+  def tags_array_for_datastream(integer)
+    array = []
+    @input["datastreams"].each {|x| array << x["tags"]}
+    array[integer]
+  end
+  
+  def current_value_for_datastream(integer)
+    array = []
+    @input["datastreams"].each {|x| array << x["current_value"]}
+    array[integer]
+  end
+  
+  def unit_label_for_datastream(integer)
+    array = []
+    @input["datastreams"].each {|x| array << x["unit"]["label"]}
+    array[integer]
+  end
+  
+  def self.time_series(input_array, input)
+    array = []
+    case input
+      when Integer
+        input_array.each {|x| array << x.current_value_for_datastream(input)}
+      when String
+        input_array.each { |x|
+          x.input["datastreams"].each { |y|
+            if y["tags"] == [input]  
+              array << y["current_value"]
+            end
+            }
+        }
+      end
+      array
+  end
 end
