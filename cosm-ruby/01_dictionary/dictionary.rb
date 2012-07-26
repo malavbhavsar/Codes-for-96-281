52ad744cff02a4c76b43a93b19279a1d4be78284
Malav bhavsar <malav.bhavsar@gmail.com>
2012-07-26 15:44:24 +0530

    Code to pass dictionary_spec.rb file
    Also removed ruby-debug-ide from Gemfile. Bundle gives some problem with
    it.

--- cosm-ruby/01_dictionary/dictionary.rb
+++ cosm-ruby/01_dictionary/dictionary.rb
@@ -1,5 +1,28 @@
 class Dictionary
 
-
+  def initialize
+    @entries = {}
+    @keywords = []
+  end
+  
+  attr_accessor :entries
+  attr_accessor :keywords
+  
+  def add(val)
+    if val.class == Hash
+      @entries.merge!(val)
+    elsif val.class == String
+      @entries.merge!(val => nil)
+    end
+    @keywords = @entries.keys.sort!
+  end
+  
+  def include?(val)
+    @keywords.include?(val)
+  end
+  
+  def find(val)
+    @entries.select{|k,v| k.include?(val)}
+  end
 
 end
